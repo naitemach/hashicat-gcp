75428249fc8962111e0c799d967e5e51743a760a
naitemach <npsnayak@gmail.com>
2023-04-30 16:32:40 +0000

    added

--- main.tf
+++ main.tf
@@ -50,10 +50,6 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-  labels = {
-    "department" = "devops"
-    "billable" = "true"
-  }
 
   boot_disk {
     initialize_params {
@@ -75,6 +71,8 @@ resource "google_compute_instance" "hashicat" {
 
   labels = {
     name = "hashicat"
+    department = "devops"
+    billable = "true"
   }
 
 }

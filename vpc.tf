4fac076c9152217da948926e86ed1646a8afa0aa
naitemach <npsnayak@gmail.com>
2023-05-01 08:59:43 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,18 @@
-network_name = "prathik-network"
-project_id = var.project
 
-subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+
+module "network" {
+  source  = "app.terraform.io/prathik-org/network/google"
+  version = "7.0.0"
+  # insert required variables here
+
+    network_name = "prathik-network"
+    project_id = var.project
+
+    subnets = [
+    {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+    }
+    ]
+}
